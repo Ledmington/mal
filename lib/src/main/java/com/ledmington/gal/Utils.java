26fbf16c5a5052eeb98fcf040b4532e2591eaa2c
Ledmington <filippo.barbari@gmail.com>
2024-03-23 13:22:49 +0100

    Updated license year

--- lib/src/main/java/com/ledmington/gal/Utils.java
+++ lib/src/main/java/com/ledmington/gal/Utils.java
@@ -1,6 +1,6 @@
 /*
 * genetic-algorithms-library - A library for genetic algorithms.
-* Copyright (C) 2023-2023 Filippo Barbari <filippo.barbari@gmail.com>
+* Copyright (C) 2023-2024 Filippo Barbari <filippo.barbari@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
